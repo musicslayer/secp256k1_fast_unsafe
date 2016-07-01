ccd243e3660c4b17f2da07f9c00fb9a3f1aaefdf
llamasoft <gravisoft@gmail.com>
2016-06-30 20:46:23 -0500

    Update benchmark harness

--- bench.sh
+++ bench.sh
@@ -1,2 +1,2 @@
-time gcc -Wall -Wno-unused-function -O2 --std=c99 -I src/ -I ./ bench_privkey.c timer.c -lgmp -o bench_privkey || exit
-time ./bench_privkey 18
+gcc -Wall -Wno-unused-function -O2 --std=c99 -I src/ -I ./ bench_privkey.c timer.c -lgmp -o bench_privkey || { echo "Benchmark compile exited with $?"; exit 1; }
+time nice -n -5 ./bench_privkey "${1:-18}"
--- bench_privkey.c
+++ bench_privkey.c
@@ -48,40 +48,49 @@ int main(int argc, char **argv) {
     printf("\n");
 
 
-
+    // Do a quick result verification
+    srand(0xDEADBEEF);
     unsigned char privkey[32] = {
         // generated using srand(31415926), first 256 calls of rand() & 0xFF
         0xb9, 0x43, 0x14, 0xa3, 0x7d, 0x33, 0x46, 0x16, 0xd8, 0x0d, 0x62, 0x1b, 0x11, 0xa5, 0x9f, 0xdd,
         0x13, 0x56, 0xf6, 0xec, 0xbb, 0x9e, 0xb1, 0x9e, 0xfd, 0xe6, 0xe0, 0x55, 0x43, 0xb4, 0x1f, 0x30
     };
 
-    printf("iterations = 2^%d (%d)\n", iter_exponent, iterations);
+    unsigned char expected[65] = {
+        0x04, 0xfa, 0xf4, 0x5a, 0x13, 0x1f, 0xe3, 0x16, 0xe7, 0x59, 0x78, 0x17, 0xf5, 0x32, 0x14, 0x0d,
+        0x75, 0xbb, 0xc2, 0xb7, 0xdc, 0xd6, 0x18, 0x35, 0xea, 0xbc, 0x29, 0xfa, 0x5d, 0x7f, 0x80, 0x25,
+        0x51, 0xe5, 0xae, 0x5b, 0x10, 0xcf, 0xc9, 0x97, 0x0c, 0x0d, 0xca, 0xa1, 0xab, 0x7d, 0xc1, 0xb3,
+        0x40, 0xbc, 0x5b, 0x3d, 0xf6, 0x87, 0xa5, 0xbc, 0xe7, 0x26, 0x67, 0xfd, 0x6c, 0xe6, 0xc3, 0x66, 0x29
+    };
 
     unsigned char pubkey[65];
+    secp256k1_privkey_to_pubkey(ctx, privkey, pubkey, 0);
+    if ( memcmp(expected, pubkey, 65) == 0 ) {
+        printf("pubkey quick test passed\n");
+    } else {
+        printf("pubkey quick test failed\n");
+        return 1;
+    }
+    printf("\n");
+
+
+
+    // Actual benchmark loop
+    printf("iterations = 2^%d (%d)\n", iter_exponent, iterations);
     clock_start = get_clock();
     for (size_t iter = 0; iter < iterations; iter++) {
+        // Randomize a byte to ensure differing code paths
+        privkey[ iter % 32 ] = rand() & 0xFF;
         secp256k1_privkey_to_pubkey(ctx, privkey, pubkey, 0);
     }
     clock_diff = get_clockdiff_s(clock_start);
 
+
+    // Benchmark results
     printf("pubkey total = %12.8f\n", clock_diff);
     printf("pubkey avg   = %12.8f\n", clock_diff / iterations);
     printf("pubkey/sec   = %12.2f\n", iterations / clock_diff);
     printf("\n");
-
-
-    // Visually inspect public key contents
-    // pubkey (u) = 04faf45a131fe316e7597817f532140d75bbc2b7dcd61835eabc29fa5d7f802551e5ae5b10cfc9970c0dcaa1ab7dc1b340bc5b3df687a5bce72667fd6ce6c36629
-    printf("pubkey (u) = ");
-    for (int i = 0; i < 65; i++) { printf("%02x", pubkey[i] & 0xFF); }
-    printf("\n");
-
-    // pubkey (c) = 03faf45a131fe316e7597817f532140d75bbc2b7dcd61835eabc29fa5d7f802551
-    printf("pubkey (c) = ");
-    secp256k1_pubkey_uncomp_to_comp(pubkey);
-    for (int i = 0; i < 33; i++) { printf("%02x", pubkey[i] & 0xFF); }
-    printf("\n");
-
 
     return 0;
 }
